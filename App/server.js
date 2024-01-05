08440bdae794b4b013e11243ea26a20242c9f3f7
drshty <drashtipatel0913@gmail.com>
2024-01-04 21:33:17 -0500

    Changed ENV variables and updated README.md according to that, also created a Apollo Client JavaScript file for making interaction with Authentication server easy

---
+++ App/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const PORT = process.env.CLIENT_SERVER_PORT
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(PORT, function () {
+   console.log(`🚀 UI started on port ${PORT}`);
+});
