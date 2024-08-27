b9a98cdd18f7f7020be0b36766934cb6bd2b9f34
drashtipatel0913 <drashtipatel0913@gmail.com>
2024-08-26 21:44:24 -0300

    addded cors and proxy to resolve request errors

--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,7 @@ const { ApolloServer } = require('apollo-server-express')
 const { ConnectToDB } = require('./db')
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:8000' }));
 
 const port = process.env.API_SERVER_PORT
 
