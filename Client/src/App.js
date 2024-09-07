05d385a7379b2b9e87f23fecfb0ade9f602fec29
drashtipatel0913 <drashtipatel0913@gmail.com>
2024-09-07 16:09:43 -0300

    Switched to HashRouter so the routes would be handled only on client-side, allowing SPA to manage routing without replying on server side

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import EmployeeList from './components/Employee/EmployeeDashboard/EmployeeList.js';
 import EmployeeDashboard from './components/Employee/EmployeeDashboard';
 import EmployeeCreate from './components/Employee/EmployeeDashboard/EmployeeCreate.js';
 import EmployeeUpdate from './components/Employee/EmployeeDashboard/EmployeeUpdate.js';
 import EmployeeDetailsPage from './components/Employee/EmployeeDashboard/EmployeeDetailsPage.js';
 
-const basename = '/EMS';
-
 function App() {
   return (
-    <BrowserRouter basename={basename}>
-      <div>
+    <HashRouter>
         <Routes>
           <Route path="/" element={<EmployeeDashboard />} />
           <Route path="/view-employee" element={<EmployeeList />} />
@@ -20,8 +17,7 @@ function App() {
           <Route path="/details/:id" element={<EmployeeDetailsPage />} />
           {/* <Route path="/retirements" element={<EmployeeRetirement />} /> */}
         </Routes>
-      </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
