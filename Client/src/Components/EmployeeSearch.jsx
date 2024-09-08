35a0fcbb31bab03bb3555428859f1b5c1e75e75a
Drashti Patel <126409186+drashtipatel0913@users.noreply.github.com>
2024-09-08 16:34:04 -0300

    Delete Client directory

--- Client/src/Components/EmployeeSearch.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class EmployeeSearch extends Component {
-   render() {
-      return (
-         <div className='text-center m-3'></div>
-      )
-   }
-}
